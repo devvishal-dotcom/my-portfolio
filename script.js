e86ce0243cdbf0e36bbcbb1736e50c7e3a6e3699
unknown <vishalkumar4606@gmail.com>
2025-04-22 19:04:22 +0530

    Added landing page project

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const btn = document.getElementById("contactBtn");
 
 btn.addEventListener("click", function () {
-  alert("Thanks for reaching out! Email: vishal@example.com");
+  alert("Thanks for reaching out! Email: vishalkumar4604@gmail.com");
 });
