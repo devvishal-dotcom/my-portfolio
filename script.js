8509f7f2aef233cc48467457ad8ec8a8948e4901
unknown <vishalkumar4606@gmail.com>
2025-04-27 21:19:30 +0530

    Updated portfolio page

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
+// Button Click Alert
 const btn = document.getElementById("contactBtn");
+if (btn) {
+  btn.addEventListener("click", function() {
+    alert("Thanks for reaching out! Email: vishalkumar4604@gmail.com");
+  });
+}
 
-btn.addEventListener("click", function () {
-  alert("Thanks for reaching out! Email: vishalkumar4604@gmail.com");
+// Navbar Scroll Effect
+window.addEventListener('scroll', function() {
+  const navbar = document.querySelector('.navbar');
+  if (navbar) {
+    if (window.scrollY > 50) {
+      navbar.classList.add('scrolled');
+    } else {
+      navbar.classList.remove('scrolled');
+    }
+  }
 });
